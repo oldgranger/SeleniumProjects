622e72c8997dad8c4dc76333a6dbd9008ed6c2ef
oldgranger <carlolegaspi12@gmail.com>
2025-06-16 20:25:19 +0800

    removed fixtures from test file

--- SauceDemoBot/test_bot.py
+++ SauceDemoBot/test_bot.py
@@ -1,32 +1,9 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from torchgen.local import parametrize
 
 from SauceDemoBot.pages.login_page import LoginPage
 from SauceDemoBot.pages.inventory_page import InventoryPage
-
-@pytest.fixture
-def driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    options.add_argument("--disable-features=PasswordCheck")
-    options.add_argument("--disable-popup-blocking")
-    options.add_experimental_option("prefs", {
-        "credentials_enable_service": False,
-        "profile.password_manager_enabled": False,
-    })
-
-    driver = webdriver.Chrome(options=options)
-    driver.implicitly_wait(5)
-    yield driver
-    driver.quit()
-
-@pytest.fixture
-def logged_in_driver(driver):
-    login_page = LoginPage(driver)
-    login_page.login("standard_user", "secret_sauce")
-    return driver
 
 @pytest.mark.parametrize("username,expect_success", [
     ("standard_user", True),
@@ -64,12 +41,6 @@ def test_add_all_to_cart_variants(driver, username, expected_count):
 
     num_added_items = len(added_items)
     assert num_added_items == expected_count, f"expected {expected_count} items, got {num_added_items} , check {added_items}" #problem user should error
-
-@pytest.fixture
-def full_cart_driver(logged_in_driver):
-    inventory_page = InventoryPage(logged_in_driver)
-    inventory_page.add_all_to_cart()
-    return logged_in_driver
 
 #parametrize()
 
