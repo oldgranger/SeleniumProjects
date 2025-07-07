a3582ad3cb8dcedb752963018b0336ec1d3fd6b2
oldgranger <carlolegaspi12@gmail.com>
2025-07-07 23:01:48 +0800

    .

--- SauceDemoBot/pages/inventory_page.py
+++ SauceDemoBot/pages/inventory_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-from SauceDemoBot.pages.base_page import BasePage
+from .base_page import BasePage
 
 class InventoryPage(BasePage):
     INVENTORY_ITEM = (By.XPATH, "//div[@class='inventory_item']")
